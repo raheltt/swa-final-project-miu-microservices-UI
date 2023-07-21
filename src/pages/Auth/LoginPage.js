142bef61b479cc172739617256ef13907ddd7397
raheltt <rteklu4@gmail.com>
2023-07-21 16:07:43 -0500

    sample login

--- src/pages/Auth/LoginPage.js
+++ src/pages/Auth/LoginPage.js
@@ -1,54 +1,77 @@
 // LoginPage.js
 
-import React, { useState } from 'react';
+import React, { useRef, useState } from 'react';
+
+
+// MATERIAL
+import { Container, Typography, TextField, Button, FormControl, InputLabel } from '@mui/material';
+import Select, { SelectChangeEvent } from '@mui/material/Select';
+import MenuItem from '@mui/material/MenuItem';
+import OutlinedInput from '@mui/material/OutlinedInput';
+
 
 const LoginPage = () => {
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
-  const [role, setRole] = useState('Admin'); // Default role is Admin
+  const userDetail = useRef({
+    username: "",
+    password: "",
+    role: ''
+  });
 
-  const handleUsernameChange = (e) => {
-    setUsername(e.target.value);
-  };
-
-  const handlePasswordChange = (e) => {
-    setPassword(e.target.value);
-  };
-
-  const handleRoleChange = (e) => {
-    setRole(e.target.value);
-  };
-
-  const handleLogin = () => {
+  const handleLogin = (e) => {
     // TODO: Implement the login logic here
     // You can use the entered username, password, and role to make API calls for authentication
     // For simplicity, let's just print the login details for now
-    console.log('Username:', username);
-    console.log('Password:', password);
-    console.log('Role:', role);
+    e.preventDefault();
+    const username = userDetail.current.username.value;
+    const password = userDetail.current.password.value;
+    const role = userDetail.current.role;
+    console.log(username, password, role)
+    
   };
 
   return (
-    <div className="login-page">
-      <h2>Login</h2>
-      <div>
-        <label>Username:</label>
-        <input type="text" value={username} onChange={handleUsernameChange} />
-      </div>
-      <div>
-        <label>Password:</label>
-        <input type="password" value={password} onChange={handlePasswordChange} />
-      </div>
-      <div>
-        <label>Role:</label>
-        <select value={role} onChange={handleRoleChange}>
-          <option value="Admin">Admin</option>
-          <option value="Teacher">Teacher</option>
-          <option value="Student">Student</option>
-        </select>
-      </div>
-      <button onClick={handleLogin}>Login</button>
-    </div>
+    <Container maxWidth="sm" style={{ marginTop: '50px' }}>
+      <Typography variant="h4" align="center" gutterBottom>
+        Login
+      </Typography>
+      <form  onSubmit={handleLogin}>
+        <TextField
+          label="Username"
+          fullWidth
+          variant="outlined"
+          margin="normal"
+          inputRef={(ref) => (userDetail.current.username = ref)}
+        />
+        <TextField
+          label="Password"
+          fullWidth
+          variant="outlined"
+          margin="normal"
+          type="password"
+          inputRef={(ref) => (userDetail.current.password = ref)}
+        />
+        <FormControl fullWidth variant="outlined" margin="normal">
+          <InputLabel id="role-select-label">Role</InputLabel>
+          <Select
+            labelId="role-select-label"
+            id="role-select"
+            value={userDetail.current.role.value}
+            onChange={(event) => (userDetail.current.role = event.target.value)}
+            label="Role"
+          >
+            <MenuItem value="">
+              <em>None</em>
+            </MenuItem>
+            <MenuItem value="admin">Admin</MenuItem>
+            <MenuItem value="teacher">Teacher</MenuItem>
+            <MenuItem value="student">Student</MenuItem>
+          </Select>
+        </FormControl>
+        <Button type="submit" variant="contained" color="primary" fullWidth>
+          Login
+        </Button>
+      </form>
+    </Container>
   );
 };
 
