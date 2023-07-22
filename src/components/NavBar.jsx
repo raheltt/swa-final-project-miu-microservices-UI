4ecfdcf7f6090acd2c7c6a452779bd3bee8e1985
raheltt <rteklu4@gmail.com>
2023-07-22 14:26:45 -0500

    sample admin board

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,16 +1,19 @@
 import * as React from 'react';
+import { Link } from 'react-router-dom';
+ 
+
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
 import IconButton from '@mui/material/IconButton';
-// import MenuIcon from '@mui/icons-material/Menu';
+import MenuIcon from '@mui/icons-material/Menu';
 
 export default function NavBar() {
   return (
     <Box sx={{ flexGrow: 1 }}>
-      {/* <AppBar position="static">
+      <AppBar position="static">
         <Toolbar>
           <IconButton
             size="large"
@@ -21,12 +24,22 @@ export default function NavBar() {
           >
             <MenuIcon />
           </IconButton>
+          
           <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            News
+          <Link to="/admins">
+            Admin
+            </Link>
           </Typography>
-          <Button color="inherit">Login</Button>
+          
+          <Button color="inherit">
+            <Link to="/admins/teachers">teachers</Link>
+            </Button>
+          <Button color="inherit">
+            <Link to="/admins/students">students</Link>
+          </Button>
+          <Button color="inherit">logout</Button>
         </Toolbar>
-      </AppBar> */}
+      </AppBar>
     </Box>
   );
 }
